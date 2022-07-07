d31e041f349e7ed21b4003b790820e9ae3ce5920
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-07-07 14:51:41 -0300

    [design patterns]

---
+++ 0x00/salario/SalarioService.java
@@ -0,0 +1,17 @@
+import java.util.UUID;
+
+public class SalarioService {
+  private final CalculadorSalarioService calculador =
+      CalculadorSalarioService.getCalculadorSalarioService();
+
+  public Double calcular(
+      double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao) {
+
+    return calculador.calcularSalarioLiquido(
+            salarioBruto, valorDescontos, valorVendas, percentualComissao);
+  }
+
+  public UUID getUuid() {
+    return this.calculador.uuid;
+  }
+}
