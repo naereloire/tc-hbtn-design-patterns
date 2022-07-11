b5916c6b872c7fe8c90567f56c7100dd67183ef3
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-07-11 13:12:37 -0300

    [design patterns] add naves

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,18 @@
+public class NaveEspacialDecorator extends NaveEspacial {
+  private final NaveEspacial naveEspacial;
+
+  public NaveEspacialDecorator(NaveEspacial naveEspacial) {
+    super(naveEspacial.getSaude(), naveEspacial.getAtaque());
+    this.naveEspacial = naveEspacial;
+  }
+
+  @Override
+  public int getSaude() {
+    return this.naveEspacial.getSaude();
+  }
+
+  @Override
+  public int getAtaque() {
+    return this.naveEspacial.getAtaque();
+  }
+}
